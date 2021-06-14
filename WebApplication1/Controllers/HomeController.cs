4d41cfb9e1316ede47e8917578ade62806d21891
Filipe Ferreiro Pereira <80466386+filipeferreiro@users.noreply.github.com>
2021-06-14 14:36:36 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var produtos = new Produtos();
+            return View(produtos);
+        }
+        [HttpPost]
+        public ActionResult Index(Produtos produtos)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Resultado", produtos);
+            }
+            return View(produtos);
+        }
+
+        public ActionResult Resultado(Produtos produtos)
+        {
+            return View(produtos);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Produtos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Produtos
+    {
+        [Required(ErrorMessage = "O campo código é obrigatório!")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório!")]
+        public string Nome { get; set; }
+
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Insira de 5 a 100 caracteres")]
+        public string Descricao { get; set; }
+
+        [Range(1,100, ErrorMessage = "Insira um peso entre 1kg e 100kg")]
+        public double Peso { get; set; }
+
+        [Required(ErrorMessage = "O campo categoria é obrigatório!")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "O campo valor unitário é obrigatório!")]
+        [Range(1, 50, ErrorMessage = "Insira um valor entre R$1,00 e R$50,00")]
+        public double ValorUni { get; set; }
+    }
+}
